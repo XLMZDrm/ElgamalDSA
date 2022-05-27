ff5b9d3cf329517e927fadee357a89860183ecbd
dimpDEV <nsovipgl@gmail.com>
2022-05-27 09:22:34 +0700

    add functions

--- src/controller/appController.js
+++ src/controller/appController.js
@@ -1,1 +1,34 @@
-let getHomePage = () => {};
+import ElGamal from "elgamal";
+let getHomePage = () => {
+  try {
+    return res.render("home");
+  } catch (error) {
+    console(error);
+    return res.redirect("/");
+  }
+};
+let elgamalEncrypt = async (
+  prime,
+  generator,
+  publicKey,
+  privateKey,
+  string
+) => {
+  const eg = new ElGamal(prime, generator, publicKey, privateKey);
+  return eg.encryptAsync(string);
+};
+let elgamalDecrypt = async (
+  prime,
+  generator,
+  publicKey,
+  privateKey,
+  string
+) => {
+  const eg = new ElGamal(prime, generator, publicKey, privateKey);
+  return eg.decryptAsync(string).toString();
+};
+export default {
+  getHomePage,
+  elgamalEncrypt,
+  elgamalDecrypt,
+};
