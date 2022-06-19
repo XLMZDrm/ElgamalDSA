a121020e15a05a434d511b5803e86261a63b47fa
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-06-19 22:53:43 +0700

    add url

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import configViewEngine from "./configs/viewEngine";
 import initWebRoute from "./route/web";
 const herokuAwake = require("heroku-awake");
 const app = express();
-const url = "https://elgamal-app.herokuapp.com/";
+const url = "https://elgamal-dsa.herokuapp.com/";
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,10 +17,3 @@ app.listen(PORT, () => {
   const time = 10;
   herokuAwake(url, time);
 });
-
-// var appRoot = require('app-root-path');
-// const WordExtractor = require("word-extractor");
-// const extractor = new WordExtractor();
-// const extracted = extractor.extract(appRoot+"/src/public/files/a.docx");
-
-// extracted.then(function(doc) { console.log(doc.getBody()); });
